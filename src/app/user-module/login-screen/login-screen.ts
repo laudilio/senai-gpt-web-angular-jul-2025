bb1cb8dde12046b5b10754a582196868c7beb517
lucas <laudilo.ferreira@bol.com.br>
2025-09-03 21:57:38 -0300

    iniciando email e senha

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-screen.html',
   styleUrl: './login-screen.css'
 })
 export class LoginScreen {
+
+  loginform: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // quando a tela inicializar.
+    // cria o campo obrigatorio de email.
+    // cria o campo obrigatorio de senha.
+
+    this.loginform = this.fb.group({
+
+      email: ["",[Validators.required]],
+      password: ["",[Validators.required]]
+    });
+  
+  }
+
+  onloginclick() {
+
+    alert("botao de login clicado");
+
+    console.log("Email", this.loginform.value.email);
+
+    console.log ("password", this.loginform.value.password);
+    
+  }
 
 }
