bb7bbe990852029758db569f37980f31624646bf
lucas <laudilo.ferreira@bol.com.br>
2025-09-04 21:59:33 -0300

    validacao de login

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -9,14 +9,14 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class LoginScreen {
 
-  loginform: FormGroup;
+  loginForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     // quando a tela inicializar.
     // cria o campo obrigatorio de email.
     // cria o campo obrigatorio de senha.
 
-    this.loginform = this.fb.group({
+    this.loginForm = this.fb.group({
 
       email: ["",[Validators.required]],
       password: ["",[Validators.required]]
@@ -24,13 +24,54 @@ export class LoginScreen {
   
   }
 
-  onloginclick() {
+  async onloginclick() {
 
     alert("botao de login clicado");
 
-    console.log("Email", this.loginform.value.email);
+    console.log("Email", this.loginForm.value.email);
 
-    console.log ("password", this.loginform.value.password);
+    console.log ("password", this.loginForm.value.password);
+
+    if (this.loginForm.value.email == "") {
+
+      alert("preencha o email.");
+      return;
+
+    }
+
+    if (this.loginForm.value.password == ""){
+
+      alert("preencha a senha.");
+      return;
+    }
+
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+
+      method: "POST", //Enviar ,
+
+      headers: {
+
+        "Content-Type" : "application/json"
+
+      },
+      body: JSON.stringify({
+
+        email:  this.loginForm.value.email,
+        password: this.loginForm.value.password,
+      })
+    });
+
+    console.log("STATUS CODE", response.status);
+
+    if (response.status == 200){
+
+      alert("esta correto");
+    } else{
+
+      alert("nao deu certo o status");
+
+
+    }
     
   }
 
